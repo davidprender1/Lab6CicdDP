aa5f3e16543421c0a055291540117c572a86199a
DAVIDPRENDERGAST-STU <g00405007@atu.ie>
2025-11-06 16:41:38 +0000

    Added class person

---
+++ src/main/java/ie/atu/lab5cicddp/Lab5CicdDpApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab5cicddp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5CicdDpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5CicdDpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab5cicddp/Lab5CicdDpApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab5cicddp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5CicdDpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
